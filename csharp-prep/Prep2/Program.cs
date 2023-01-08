106de43affceb13d0d3fd50df95fefa3c7066c97
Alex Blad <alexander.m.blad@gmail.com>
2023-01-07 17:02:38 -0700

    Finished assignment Prep 2

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,43 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        Console.WriteLine("Hello, what is your grade percentage in this class? ");
+        string answer= Console.ReadLine();
+        int percent= int.Parse(answer);
+
+        string letter = "";
+
+        if (percent>89)
+        {
+            letter="A";
+        }
+        else if (percent>79)
+        {
+            letter="B";
+        }
+        else if (percent>69)
+        {
+            letter="C";
+        }
+        else if (percent>59)
+        {
+            letter="D";
+        }
+        else
+        {
+            letter="F";
+        }
+
+        Console.WriteLine($"Your grade is: {letter}");
+
+        if (percent>69)
+        {
+            Console.WriteLine("Congratulations, you passed!");
+        }
+        else
+        {
+            Console.WriteLine("Sorry you goober, better luck next time!");
+        }
+
     }
 }
